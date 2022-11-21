f99e13588172583c2a00a3f1e160f2a25a838d8a
Pattadon Loyprasert <pattadon2546pn@gmail.com>
2022-11-21 19:04:39 +0700

    Change external type file to json.

--- main.py
+++ main.py
@@ -1,13 +1,10 @@
-import csv
+import json
 
 
 def read_external_file(file_name):
-    data_list = []
-    with open(file_name) as temp_var:
-        file = csv.DictReader(temp_var)
-        for each_line in file:
-            data_list.append(each_line)
-    return data_list
+    with open(file_name, 'r') as file:
+        data_file = json.load(file)
+        return data_file
 
 
 def create_an_account(filename):
@@ -15,15 +12,32 @@ def create_an_account(filename):
     print('Hi this is registration form :)')
     print('===============================')
     username = input('What should we call you?: ')
+    # Check if the username has been used.
+    with open(filename, 'r') as file:
+        data_file = json.load(file)
+        while username in data_file:
+            print('Sorry, this username has already been used.')
+            username = input('What should we call you?: ')
+
     print('===============================')
     print(f'Hi {username} Nice to meet ya :)')
     print('===============================')
-    password = input('And what will  your password be?: ')
-    with open(filename, 'a', encoding='utf-8') as file:
-        file.write('\n')
-        file.write(username)
-        file.write(' ')
-        file.write(password)
+    password = input('And what will your password be?: ')
+    print('PETFECTOOOO!!')
+    address = input('Now the address for your shipping: ')
+    tel = input('Your telephone number please:) ')
+    new_data = {
+        username: {
+            "password": password,
+            "address": address,
+            "telephone": tel
+        }
+    }
+    with open(filename, 'r') as file:
+        data_file = json.load(file)
+        data_file.update(new_data)
+    with open(filename, 'w') as file:
+        json.dump(data_file, file, indent=4)
 
 
 def check_y_n(choice):
@@ -52,15 +66,15 @@ def login(data):
         password = input('And password?: ')
 
 
-def update_customer_data(data, username, password, address, telephone):
-    with open(data) as file:
-        data_file = csv.DictReader(file)
-        for each_file in data_file:
-            if each_file['username'] == username:
-                each_file['username'] = username
-                each_file['password'] = password
-                each_file['address'] = address
-                each_file['telephone'] = telephone
+# def update_customer_data(data, username, password, address, telephone):
+#     with open(data) as file:
+#         data_file = csv.DictReader(file)
+#         for each_file in data_file:
+#             if each_file['username'] == username:
+#                 each_file['username'] = username
+#                 each_file['password'] = password
+#                 each_file['address'] = address
+#                 each_file['telephone'] = telephone
 
 
 class Customer:
@@ -71,9 +85,9 @@ class Customer:
         print(f'Hi {self.__username} welcome to my shop :)')
 
 
-user_file = '../Drug4U/User_file/User_data.csv'
+user_file = '../Drug4U/User_file/User_data.json'
 customer_data = (read_external_file(user_file))
-admin_data = read_external_file('../Drug4U/Admin_file/Admin_data.csv')
+admin_data = read_external_file('../Drug4U/Admin_file/Admin_data.json')
 print('''
 ===========================
 Welcome to My DRUG4U Shop!
@@ -90,11 +104,11 @@ if check_wheter_customer == 'n' or check_wheter_customer == 'N':
     print('============================')
     username, password = login(admin_data)
 
-if check_wheter_customer == 'y' or check_wheter_customer == 'Y':
+if check_wheter_customer.lower() == 'y':
     print('Do you have an account?')
     check_account = input('(y/n): ')
     check_account = check_y_n(check_account)
-    if check_account == 'n' or check_account == 'N':
+    if check_account.lower() == 'n':
         create_an_account(user_file)
         customer_data = (read_external_file(user_file))
 
